00ef83498f3d97f670947cc2fce663d1144b0d2c
jung <joel.ung@soprasteria.com>
2018-05-17 19:27:31 +0200

    feat: reference a une methode avec java 8

---
+++ src/main/java/ung/joel/trainingjava/referencemethode/ReferenceMethode.java
@@ -0,0 +1,19 @@
+package ung.joel.trainingjava.referencemethode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReferenceMethode {
+
+    public static Integer POURCENTAGE = 10;
+
+    public static void main(String[] args) {
+        // Des nombres
+        List<Integer> desNombres = Arrays.asList(47,5,45,18,33,24,48);
+
+        // On a besoin de rechercher parmis ces nombres, ceux qui sont proches de 50
+
+        // On affiche / récupère le résultat
+    }
+}
